65f11741064db9197471ef50f221e75dffe8cc72
YONGMINJO1 <iy0594@naver.com>
2025-11-01 01:04:32 +0900

    feat: Application 구현 프로그램 완성
    - main 메서드에서 객체 생성 및 의존성 주입
    - Controller를 통해서 로또게임 실행

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.utils.LottoGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        LottoGenerator lottoGenerator = new LottoGenerator();
+
+        LottoController controller = new LottoController(
+                inputView,
+                outputView,
+                lottoGenerator
+        );
+        controller.run();
     }
 }
