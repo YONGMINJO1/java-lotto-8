849e1e9dc758924696f12b5350f234d9ed759768
YONGMINJO1 <iy0594@naver.com>
2025-11-02 16:51:04 +0900

    refactor: Lotto 클래스 리팩토링
    - validate 메서드 내부에 validateSize, validateDuplicate, validateRange
      각각의 메서드 생성
    - validateNumberRange 메서드 추가로 indent 개선(3->2)

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -7,6 +7,10 @@ import java.util.List;
 import java.util.Set;
 
 public class Lotto {
+    private static final int LOTTO_NUMBER_SIZE = 6;
+    private static final int MIN_LOTTO_NUMBER = 1;
+    private static final int MAX_LOTTO_NUMBER = 45;
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -15,19 +19,38 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
-        }
+        validateSize(numbers);
+        validateDuplicate(numbers);
+        validateRange(numbers);
+    }
 
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_NUMBER_SIZE) {
+            throw new IllegalArgumentException(
+                    String.format("[ERROR] 로또 번호는 %d개여야 합니다.", LOTTO_NUMBER_SIZE)
+            );
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
         Set<Integer> uniqueNumbers = new HashSet<>(numbers);
         if (uniqueNumbers.size() != numbers.size()) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 중복될 수 없습니다.");
         }
+    }
 
+    private void validateRange(List<Integer> numbers) {
         for (Integer number : numbers) {
-            if (number < 1 || number > 45) {
-                throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 범위를 입력할 수 있습니다.");
-            }
+            validateNumberRange(number);
+        }
+    }
+
+    private void validateNumberRange(int number) {
+        if (number < MIN_LOTTO_NUMBER || number > MAX_LOTTO_NUMBER) {  // depth 2
+            throw new IllegalArgumentException(
+                    String.format("[ERROR] 로또 번호는 %d~%d 범위를 입력할 수 있습니다.",
+                            MIN_LOTTO_NUMBER, MAX_LOTTO_NUMBER)
+            );
         }
     }
 
@@ -39,6 +62,5 @@ public class Lotto {
         List<Integer> sorted = new ArrayList<>(numbers);
         Collections.sort(sorted);
         return sorted;
-        //return new ArrayList<>(numbers);
     }
 }
